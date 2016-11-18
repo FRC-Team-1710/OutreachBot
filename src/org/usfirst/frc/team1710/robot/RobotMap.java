fda1ae8c976312df65c43c65a13154639b6c69fd
LukeCu <luke.c@ravenengineer.org>
2016-11-17 19:53:13 -0600

    Set up RobotMap
    yay

--- src/org/usfirst/frc/team1710/robot/RobotMap.java
+++ src/org/usfirst/frc/team1710/robot/RobotMap.java
@@ -1,5 +1,11 @@
 package org.usfirst.frc.team1710.robot;
 
-public class RobotMap {
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Talon;
 
+public class RobotMap {
+RobotDrive driveTrain;
+Joystick drive, mech;
+Talon hopper, shooter;
 }
