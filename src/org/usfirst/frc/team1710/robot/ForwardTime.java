1c115a4357aec5c1977d157a6ab0a6efed8db903
LukeCu <luke.c@ravenengineer.org>
2016-12-05 15:40:24 -0600

    Made ForwardTime

---
+++ src/org/usfirst/frc/team1710/robot/ForwardTime.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team1710.robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class ForwardTime extends Command {
+	public double powerPublic;
+	public double timePublic;
+	int count;
+	boolean done;
+    public ForwardTime(double power, double time) {
+        powerPublic = power;
+        timePublic = time;
+    }
+
+    protected void initialize() {
+    	
+    }
+
+    protected void execute() {
+    	if (count <= timePublic/20) {
+    		count++;
+    		done = false;
+    	}
+    	else {
+    		done = true;
+    	}
+    }
+
+    protected boolean isFinished() {
+    
+        return done;
+    }
+
+    protected void end() {
+    	
+    }
+
+    protected void interrupted() {
+    	
+    }
+}
