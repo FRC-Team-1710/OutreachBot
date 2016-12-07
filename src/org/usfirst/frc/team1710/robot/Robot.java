f0662669284348c44df0adac5d1431891d6a9355
Jake Herman <thisguyherm@gmail.com>
2016-12-07 14:34:58 -0600

    auto selection code

--- src/org/usfirst/frc/team1710/robot/Robot.java
+++ src/org/usfirst/frc/team1710/robot/Robot.java
@@ -5,31 +5,24 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
 public class Robot extends IterativeRobot {
     final String defaultAuto = "Default";
-    final String customAuto = "My Auto";
+    final String lukeAuto = "Luke Auto";
+    final String pennAuto = "Penn Auto";
     String autoSelected;
     SendableChooser chooser;
     double turn, move;
-	
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
+    Command autonomousCommand;
+
     public void robotInit() {
         chooser = new SendableChooser();
         chooser.addDefault("Default Auto", defaultAuto);
-        chooser.addObject("My Auto", customAuto);
+        chooser.addObject("Luke Auto", lukeAuto);
+        chooser.addObject("Penn Auto", pennAuto);
         SmartDashboard.putData("Auto choices", chooser);
         RobotMap.driveTrain = new RobotDrive(3, 2, 4, 5);
         RobotMap.drive = new Joystick(0);
@@ -38,48 +31,28 @@ public class Robot extends IterativeRobot {
         move = 0;
     }
     
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
-	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
-	 * Dashboard, remove all of the chooser code and uncomment the getString line to get the auto name from the text box
-	 * below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the switch structure below with additional strings.
-	 * If using the SendableChooser make sure to add them to the chooser code above as well.
-	 */
+
     public void autonomousInit() {
     	autoSelected = (String) chooser.getSelected();
-//		autoSelected = SmartDashboard.getString("Auto Selector", defaultAuto);
 		System.out.println("Auto selected: " + autoSelected);
+		if(autoSelected == lukeAuto) {
+			autonomousCommand = new LukesAuto();
+		} else if (autoSelected == pennAuto) {
+			autonomousCommand = new PennsAuto();
+		}
     }
 
-    /**
-     * This function is called periodically during autonomous
-     */
+
     public void autonomousPeriodic() {
-    	switch(autoSelected) {
-    	case customAuto:
-        //Put custom auto code here   
-            break;
-    	case defaultAuto:
-    	default:
-    	//Put default auto code here
-            break;
-    	}
+
     }
 
-    /**
-     * This function is called periodically during operator control
-     */
     public void teleopPeriodic() {
         turn = RobotMap.drive.getRawAxis(4);
         move = RobotMap.drive.getRawAxis(1);
         Drive.arcadeDrive(move, turn);
     }
-    
-    /**
-     * This function is called periodically during test mode
-     */
+
     public void testPeriodic() {
     
     }
