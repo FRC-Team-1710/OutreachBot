932864ab0319495cf09f4b2e0f0a50fb08afc3eb
Jake Herman <thisguyherm@gmail.com>
2017-01-13 15:43:01 -0600

    added vsion code decreased driving speed
    it got a little bit dangerous

--- src/org/usfirst/frc/team1710/robot/Drive.java
+++ src/org/usfirst/frc/team1710/robot/Drive.java
@@ -9,8 +9,12 @@ public class Drive extends Subsystem {
     public void initDefaultCommand() {
     	RobotMap.driveTrain.arcadeDrive(0,0);
     }
-    public static void arcadeDrive(double drive, double turn){
-    	RobotMap.driveTrain.arcadeDrive(drive * -1, turn * -1);
+    public static void arcadeDrive(double drive, double turn, boolean boost){
+    	if(boost) {
+    		RobotMap.driveTrain.arcadeDrive(drive * -1, turn * -1);
+    	}else{
+    		RobotMap.driveTrain.arcadeDrive(drive * -0.6, turn * -0.6);
+    	}
     }
 }
 
--- src/org/usfirst/frc/team1710/robot/Robot.java
+++ src/org/usfirst/frc/team1710/robot/Robot.java
@@ -1,34 +1,40 @@
 
 package org.usfirst.frc.team1710.robot;
 
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
     SendableChooser chooser;
     double turn, move, shooter, hopper;
+    boolean speedBoost;
     Command autonomousCommand;
     
+    NetworkTable table;
     
     public void robotInit() {
         chooser = new SendableChooser();
         SmartDashboard.putData("Auto choices", chooser);
         RobotMap.driveTrain = new RobotDrive(3, 2, 4, 5);
         RobotMap.drive = new Joystick(0);
-        Drive.arcadeDrive(0, 0);
+        Drive.arcadeDrive(0, 0, speedBoost);
         RobotMap.hopper = new Talon(0);
         RobotMap.shooter = new Talon(1);
         turn = 0;
         move = 0;
         //hopper = 0;
         //shooter = 0;
-        
+        CameraServer camera = CameraServer.getInstance();
+        camera.setQuality(30);
+        camera.startAutomaticCapture("cam0");
     }
     
 
@@ -45,11 +51,13 @@ public class Robot extends IterativeRobot {
     public void teleopPeriodic() {
         turn = RobotMap.drive.getRawAxis(4);
         move = RobotMap.drive.getRawAxis(1);
-        Drive.arcadeDrive(move, turn);
+        speedBoost = RobotMap.drive.getRawButton(5);
+        Drive.arcadeDrive(move, turn, speedBoost);
         hopper = RobotMap.drive.getRawAxis(3);
         RobotMap.hopper.set(hopper*-1);
         shooter = RobotMap.drive.getRawAxis(2);
         RobotMap.shooter.set(shooter*-.75);
+        
     }
     
 
