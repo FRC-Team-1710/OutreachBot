50fcda3ca086524c417e362c3fb102aef9dc1255
Jake Herman <thisguyherm@gmail.com>
2017-01-19 10:45:19 -0600

    why isnt the navx working im gonna go nuts

--- src/org/usfirst/frc/team1710/robot/RobotMap.java
+++ src/org/usfirst/frc/team1710/robot/RobotMap.java
@@ -2,13 +2,14 @@ package org.usfirst.frc.team1710.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import com.kauailabs.navx.frc.AHRS;
+import com.kauailabs.navx.frc.*;
 
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Talon;
 
 public class RobotMap {
-public static RobotDrive driveTrain;
-public static Joystick drive, mech;
-public static Talon hopper, shooter;
-public static AHRS navx;
+	public static RobotDrive driveTrain;
+	public static Joystick drive, mech;
+	public static Talon hopper, shooter;
+	public static AHRS navx;
 }
